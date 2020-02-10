df2f677d65fa1ffcabb328066a5349812d5cca9e
Andy Nguyen <apnguyen11@yahoo.com>
2020-02-10 14:21:20 -0600

    initial set up with bootstrap and firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAbI8kyMa270jMqfDUrPFnkuurM7-JJf5A",
+    authDomain: "oshop-441fe.firebaseapp.com",
+    databaseURL: "https://oshop-441fe.firebaseio.com",
+    projectId: "oshop-441fe",
+    storageBucket: "oshop-441fe.appspot.com",
+    messagingSenderId: "1073493938473",
+    appId: "1:1073493938473:web:dc03a5fbf1740a35236738",
+    measurementId: "G-XMFM3NYGWQ"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAbI8kyMa270jMqfDUrPFnkuurM7-JJf5A",
+    authDomain: "oshop-441fe.firebaseapp.com",
+    databaseURL: "https://oshop-441fe.firebaseio.com",
+    projectId: "oshop-441fe",
+    storageBucket: "oshop-441fe.appspot.com",
+    messagingSenderId: "1073493938473",
+    appId: "1:1073493938473:web:dc03a5fbf1740a35236738",
+    measurementId: "G-XMFM3NYGWQ"
+  }
 };
 
 /*
