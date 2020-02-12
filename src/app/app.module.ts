589e3946348bfdf651abbe6ae2db1764d4bd14e4
Andy Nguyen <apnguyen11@yahoo.com>
2020-02-12 16:06:47 -0600

    extracting a service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { AdminProductsComponent } from './admin/admin-products/admin-products.co
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -54,7 +55,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
       
     ])
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
